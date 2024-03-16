b3fcbc61f96db41e778492a28d09be73b3b92518
Muhammad Danish <160287115+mddanishmeta@users.noreply.github.com>
2024-03-16 15:05:58 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([{
+        message: "Enter First Number", type: "number", name: "firstNumber"
+    },
+    { message: "Enter second Number", type: "number", name: "secondNumber" },
+    { message: "slect one of the Operation", type: "list", name: "operator", choices: ["Addition", "Substraction",
+            "Multiplcation", "Division"],
+    }]);
+//conitional statement
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "Substraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "Multiplcation") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else
+    console.log("please slect valid operator");
